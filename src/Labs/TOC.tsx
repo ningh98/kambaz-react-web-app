194ada9ee755b5c6b0ed11d21f4e67e4995bd95a
Ninghui Cai <ninghuicai@Ninghuis-MacBook-Pro.local>
2025-01-09 16:33:48 -0800

    add github link & name

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 export default function TOC() {
     return (
       <ul>
+        <li><a href="https://github.com/ningh98/kambaz-react-web-app" id="wd-github">My Github repository</a></li>
         <li><Link to="/Labs">Labs</Link></li>
         <li><Link to="/Labs/Lab1">Lab 1</Link></li>
         <li><Link to="/Labs/Lab2">Lab 2</Link></li>
