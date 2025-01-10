99a6d445b48a95dadc6f07f266602fb2e074edb1
Ninghui Cai <ninghuicai@Ninghuis-MacBook-Pro.local>
2025-01-09 21:31:00 -0800

    second commit

--- src/Kambaz/Courses/Modules/index.tsx
+++ src/Kambaz/Courses/Modules/index.tsx
@@ -1,16 +1,40 @@
 export default function Modules() {
     return (
       <div>
-        {/* Implement Collapse All button, View Progress button, etc. */}
+        <button>Collapse All</button>
+        <button>View Progress</button>
+        <select name="publishAll" id="wd-publish">
+              <option value="publish-all">Publish All</option>
+        </select>
+        <button>+ Module</button>
         <ul id="wd-modules">
           <li className="wd-module">
-            <div className="wd-title">Week 1</div>
+            <div className="wd-title">Week 1, Lecture 1 - Course Introduction, Syllabus, Agenda</div>
             <ul className="wd-lessons">
               <li className="wd-lesson">
                 <span className="wd-title">LEARNING OBJECTIVES</span>
                 <ul className="wd-content">
                   <li className="wd-content-item">Introduction to the course</li>
                   <li className="wd-content-item">Learn what is Web Development</li>
+                </ul>
+              </li>
+            </ul>
+            <ul className="wd-readings">
+              <li className="wd-reading">
+                <span className="wd-title">READING</span>
+                <ul className="wd-content">
+                  <li className="wd-content-item">Full Stack Developer - Chapter 1 - Introductior</li>
+                  <li className="wd-content-item">Full Stack Developer - Chapter 2 - Creating Us</li>
+                </ul>
+              </li>
+            </ul>
+            <ul className="wd-slides">
+              <li className="wd-slide">
+                <span className="wd-title">SLIDES</span>
+                <ul className="wd-content">
+                  <li className="wd-content-item">Introduction to Web Development</li>
+                  <li className="wd-content-item">Creating an HTTP server with Node.js</li>
+                  <li className="wd-content-item">Creating a React Application</li>
                 </ul>
               </li>
             </ul>
--- src/Kambaz/Dashboard.tsx
+++ src/Kambaz/Dashboard.tsx
@@ -6,20 +6,116 @@ export default function Dashboard() {
       <h2 id="wd-dashboard-published">Published Courses (12)</h2> <hr />
       <div id="wd-dashboard-courses">
         <div className="wd-dashboard-course">
-          <Link to="/Kambaz/Courses/1234/Home"
-                className="wd-dashboard-course-link" >
-            <img src="/images/reactjs.webp" width={200} />
+          <Link
+            to="/Kambaz/Courses/1234/Home"
+            className="wd-dashboard-course-link"
+          >
+            <img src="/images/reactjs.webp" width={200} height={150} />
             <div>
               <h5> CS1234 React JS </h5>
               <p className="wd-dashboard-course-title">
-                Full Stack software developer  </p>
+                Full Stack software developer{" "}
+              </p>
               <button> Go </button>
             </div>
           </Link>
         </div>
-        <div className="wd-dashboard-course"> ... </div>
-        <div className="wd-dashboard-course"> ... </div>
+        <div className="wd-dashboard-course">
+          <Link
+            to="/Kambaz/Courses/1234/Home"
+            className="wd-dashboard-course-link"
+          >
+            <img src="/images/python.jpeg" width={200} height={150} />
+            <div>
+              <h5> CS1235 Python </h5>
+              <p className="wd-dashboard-course-title">
+                Python software developer{" "}
+              </p>
+              <button> Go </button>
+            </div>
+          </Link>
+        </div>
+        <div className="wd-dashboard-course">
+          {" "}
+          <Link
+            to="/Kambaz/Courses/1234/Home"
+            className="wd-dashboard-course-link"
+          >
+            <img src="/images/java.png" width={200} height={150} />
+            <div>
+              <h5> CS1236 Java </h5>
+              <p className="wd-dashboard-course-title">
+                Java software developer{" "}
+              </p>
+              <button> Go </button>
+            </div>
+          </Link>{" "}
+        </div>
+        <div className="wd-dashboard-course">
+          {" "}
+          <Link
+            to="/Kambaz/Courses/1234/Home"
+            className="wd-dashboard-course-link"
+          >
+            <img src="/images/c.png" width={200} height={150} />
+            <div>
+              <h5> CS1237 C </h5>
+              <p className="wd-dashboard-course-title">
+                C software developer{" "}
+              </p>
+              <button> Go </button>
+            </div>
+          </Link>{" "}
+        </div>
+        <div className="wd-dashboard-course">
+          {" "}
+          <Link
+            to="/Kambaz/Courses/1234/Home"
+            className="wd-dashboard-course-link"
+          >
+            <img src="/images/c++.png" width={200} height={150} />
+            <div>
+              <h5> CS1238 C++ </h5>
+              <p className="wd-dashboard-course-title">
+                C++ software developer{" "}
+              </p>
+              <button> Go </button>
+            </div>
+          </Link>{" "}
+        </div>
+        <div className="wd-dashboard-course">
+          {" "}
+          <Link
+            to="/Kambaz/Courses/1234/Home"
+            className="wd-dashboard-course-link"
+          >
+            <img src="/images/math.jpeg" width={200} height={150} />
+            <div>
+              <h5> CS1239 Math </h5>
+              <p className="wd-dashboard-course-title">
+                Introduction of Math{" "}
+              </p>
+              <button> Go </button>
+            </div>
+          </Link>{" "}
+        </div>
+        <div className="wd-dashboard-course">
+          {" "}
+          <Link
+            to="/Kambaz/Courses/1234/Home"
+            className="wd-dashboard-course-link"
+          >
+            <img src="/images/art.jpeg" width={200} height={150} />
+            <div>
+              <h5> CS1222 Art </h5>
+              <p className="wd-dashboard-course-title">
+                Introduction of Art{" "}
+              </p>
+              <button> Go </button>
+            </div>
+          </Link>{" "}
+        </div>
       </div>
     </div>
-);}
-
+  );
+}
