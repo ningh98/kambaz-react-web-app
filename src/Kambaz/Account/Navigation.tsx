a73354d53b8b4498a59fb97fcd41fcb30387972d
Ninghui Cai <ninghuicai@Ninghuis-MacBook-Pro.local>
2025-03-16 12:47:53 -0700

    a6 first commit

--- src/Kambaz/Account/Navigation.tsx
+++ src/Kambaz/Account/Navigation.tsx
@@ -4,6 +4,8 @@ import { useSelector } from "react-redux";
 export default function AccountNavigation() {
   const { currentUser } = useSelector((state: any) => state.accountReducer);
   const links = currentUser ? ["Profile"] : ["Signin", "Signup"];
+  const active = (path: string) => (pathname.includes(path) ? "active" : "");
+
   const { pathname } = useLocation();
   
   return (
@@ -13,10 +15,12 @@ export default function AccountNavigation() {
         key={link}
         to={`/Kambaz/Account/${link}`}
         className = {`list-group-item text-danger border border-0
-                      ${pathname.includes(link) && "active"}`}>
+                      ${active(link)}`}>
        {link}
       </Link>
-
       ))}
+      
+      {currentUser && currentUser.role === "ADMIN" && (
+       <Link to={`/Kambaz/Account/Users`} className={`list-group-item border border-0 ${active("Users")}`}> Users </Link> )}
     </div>
 );}
